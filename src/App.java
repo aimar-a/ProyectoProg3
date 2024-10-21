e2bf87d9ee1243d22ea52eee8f92a0ea1f38652d
gimar <aimar.a@opendeusto.es>
2024-10-21 13:28:35 +0200

    deiV

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import GUI.LogIn;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("ola, World!");
+        System.out.println("david trueba!");
         LogIn login = new LogIn();
         login.showLoginWindow();
     }
