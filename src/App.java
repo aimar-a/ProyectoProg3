4aee12d7a48400ce5537e66dde3757d1f08d2c5c
david.trueba <david.trueba@10.164.55.198>
2024-10-21 13:02:25 +0200

    aja

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import GUI.LogIn;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hel, World!");
         LogIn login = new LogIn();
         login.showLoginWindow();
     }
