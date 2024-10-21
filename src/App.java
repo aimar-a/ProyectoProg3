a10840963a04daa4f987ab187e5a3b4f83a97bb5
c.llata <c.llata@10.164.52.192>
2024-10-21 13:26:36 +0200

    pitiyggfhvn

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import GUI.LogIn;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hel, World!");
+        System.out.println("ola, World!");
         LogIn login = new LogIn();
         login.showLoginWindow();
     }
