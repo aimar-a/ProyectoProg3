6c113962e15c624a19f1aa0cd2b24fa8c0183c51
aimar-a <153600724+aimar-a@users.noreply.github.com>
2024-10-23 10:36:47 +0200

    test

--- src/App.java
+++
@@ -1,9 +0,0 @@
-import GUI.LogIn;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("ola, World!");
-        LogIn login = new LogIn();
-        login.showLoginWindow();
-    }
-}
