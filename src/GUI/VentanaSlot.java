6403d838e7ae3bd505359e2ec75d81e485959355
c.llata <c.llata@10.164.49.251>
2024-10-30 09:07:16 +0100

    ventana slot

---
+++ src/GUI/VentanaSlot.java
@@ -0,0 +1,21 @@
+package GUI;
+
+import java.awt.BorderLayout;
+
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class VentanaSlot extends JFrame {
+	public VentanaSlot() {
+	        setTitle("Slot");
+	        setSize(400, 300);
+	        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	        setLocationRelativeTo(null);
+	        JLabel label = new JLabel("¡Bienvenido a los Slots!", SwingConstants.CENTER);
+	        add(label, BorderLayout.CENTER);
+
+	        setVisible(true);
+	    }
+	}
